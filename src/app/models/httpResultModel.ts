9ed25b4f0655cd63677d1f51ae68bfa8306afd9d
israelgoomes <rael_goomes@hotmail.com>
2019-06-09 11:15:56 -0300

    Criação do modelo HttpResult

---
+++ src/app/models/httpResultModel.ts
@@ -0,0 +1,8 @@
+//reescrevendo um método httpClient do próprio ionic, para afzer tratamento, se o cara tá conectado ou não, já colocar o loading automaticamente, etc..
+export class HttpResultModel{
+    //a informação que virá do resultado
+    data: any;
+    //para ver se já veio com sucesso, ou não (error)
+    succes: boolean;
+    err: any;
+}
